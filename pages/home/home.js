9aa284d7d0536d0399800344f6c9f57d96a8f7a2
sunshuai <sunshuai.edu@gmail.com>
2021-05-30 11:12:46 +0800

    overwrite the input name with the real name

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,7 +7,13 @@ Page({
   },
   goPreview() {
     wx.scanCode({
-      success: () => {
+      success: (res) => {
+        let realGateName = this.getGateNameFromUrl(res.result)
+        //如果从二维码中获取到了大门信息，则覆盖输入的大门名称
+        if (realGateName) {
+          this.data.gatename = realGateName
+        }
+
         wx.redirectTo({
           url: `/pages/preview/preview?name=${this.data.name}&stuid=${this.data.stuid}&gatename=${this.data.gatename}`
         })
@@ -26,5 +32,48 @@ Page({
         placeholderFigureVisible: false
       })
     }, 3000)
+  },
+  
+  getGateNameFromUrl(url) {
+    let matches = url.match(/entrance_id=(\d+)&/)
+    if (!matches) return ''
+    let gateName = ''
+    switch (matches[1]) {
+      case '1':
+        gateName = '南湖校区北门'
+        break
+      case '2':
+        gateName = '南湖校区南门'
+        break
+      case '3':
+        gateName = '南湖校区西门'
+        break
+      case '4':
+        gateName = '南湖校区东门'
+        break
+      case '5':
+        gateName = '浑南校区东门'
+        break
+      case '6':
+        gateName = '南湖校区测试'
+        break
+      case '7':
+        gateName = '南湖校区小北门'
+        break
+      case '8':
+        gateName = '浑南校区南门'
+        break
+      case '9':
+        gateName = '浑南校区西门'
+        break
+      case '10':
+        gateName = '浑南校区北门'
+        break
+      case '11':
+        gateName = '沈河校区'
+        break
+    }
+    return gateName
   }
+
 })
